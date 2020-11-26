97370ded1042a58725955ceba3515f85cd7e40d4
Iqbalmubarak <48642060+Iqbalmubarak@users.noreply.github.com>
2020-11-26 08:07:50 +0700

    connect.php

--- connect.php
+++ connect.php
@@ -3,6 +3,6 @@
 	$user = "oxlvpglpeheqjd";
 	$pass = "a1638f74b0c9ea8687aa5ec65e72ae3104e7eec1c8b57d20c811c2fdd436b483";
 	$port = "5432";
-	$dbname = "1";
+	$dbname = "de2lo4f4skqep2";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
